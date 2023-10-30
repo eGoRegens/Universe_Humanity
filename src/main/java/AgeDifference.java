758881ed0a2c86f8908c26e76669ee2a2b800b71
eGoRegens <125861111+eGoRegens@users.noreply.github.com>
2023-10-30 20:31:27 +0400

    add task 5

---
+++ src/main/java/AgeDifference.java
@@ -0,0 +1,11 @@
+public class AgeDifference {
+    public static void main(String[] args) {
+        long ageOfTheUniverse = 13000000000L;
+        long ageOfTheHumans = 100000;
+        long ageDifference = ageOfTheUniverse / ageOfTheHumans;
+
+        System.out.println("Возраст вселенной " + ageOfTheUniverse + " лет.");
+        System.out.println("Возраст человечества " + ageOfTheHumans + " лет.");
+        System.out.println("Возраст вселенной больше в " + ageDifference + " раз больше человечества.");
+    }
+}
